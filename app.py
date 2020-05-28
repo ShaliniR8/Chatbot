2405dcab0f0d2065a39f06ea9a561e512a9b40f6
ShaliniR8 <1705820@kiit.ac.in>
2020-05-28 16:46:29 +0530

    Separated application into login page, home page and chat

--- app.py
+++ app.py
@@ -2,18 +2,16 @@ from flask import Flask, render_template, request
 from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 from chatbot import chatbot
 
+#chatterbotcorpus training
 '''
-conv= open("test.txt","r").readlines()
-trainer=ListTrainer(chatbot)
-trainer.train(conv)
 trainer=ChatterBotCorpusTrainer(chatbot)
 trainer.train("chatterbot.corpus.english")
-
 '''
 
 app = Flask(__name__)
 
 
+#list training
 '''
 conv1=open("train1.txt","r").readlines()
 trainer2=ListTrainer(english_bot)
@@ -27,6 +25,10 @@ def home():
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
 
 @app.route("/get")
 def get_bot_response():
