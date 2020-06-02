14298800947653d1098e89b8907d518265d1a687
ShaliniR8 <1705820@kiit.ac.in>
2020-06-02 20:03:10 +0530

    minor changes

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
+#1. Login
+#2. Be directed to website
+#3. Be directed to Chat application
+#4. Get response
+
 from flask import Flask, render_template, request
 from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
-from chatbot import chatbot
+from chat2 import chatbot
 
 #chatterbotcorpus training
 '''
 trainer=ChatterBotCorpusTrainer(chatbot)
 trainer.train("chatterbot.corpus.english")
 '''
-
-app = Flask(__name__)
-
 
 #list training
 '''
@@ -18,22 +20,32 @@ trainer2=ListTrainer(english_bot)
 trainer2.train(conv1)
 '''
 
+app = Flask(__name__)
+
+
+#login
 @app.route("/")
 def home():
     return render_template("home.html")
 
+#website
 @app.route("/index")
 def index():
     return render_template("index.html")
 
+#chat
 @app.route("/chat")
 def chat():
     return render_template("chat.html")
 
+#get response
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(chatbot.get_response(userText))
+    if userText.strip()=='Bye' or userText.strip()=='bye':
+        return str("Bye!")
+    else:
+        return str(chatbot.get_response(userText))
 
 
 if __name__ == "__main__":
--- chat2.py
+++ chat2.py
@@ -1,18 +1,25 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
-chatbot=ChatBot("Alex")
-trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train(
-    "chatterbot.corpus.english"
-)
+chatbot=ChatBot("Alex",storage_adapter="chatterbot.storage.MongoDatabaseAdapter")
 
-while True:
-    userInput=input("User:")
-    if userInput.strip()=="Bye" or userInput.strip()=="bye":
-        print("Alex:Bye!")
-        break;
-    else:
-        response=trainer.get_response(userInput)
-        print(response)
 
+trainer=ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.english")
+
+
+def converse():
+    while True:
+        userInput=input("User:")
+        if userInput.strip()=="Bye" or userInput.strip()=="bye":
+            print("Alex:Bye!")
+            break;
+        else:
+            response=chatbot.get_response(userInput) 
+            print(response)
+
+
+if __name__=="__main__":
+    i=input("Enter START to start chatting\n")
+    if i.strip()=="START":
+        converse()
